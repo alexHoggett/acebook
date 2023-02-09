3916dcf30935a25ba103b9f9c16d1ba410a22087
MichalSmyk <michal.smyk39@gmail.com>
2023-02-09 10:05:18 +0000

    changes

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -22,6 +22,15 @@ const PostsController = {
       res.status(201).json({ message: 'OK', token: token });
     });
   },
+  Delete: (req, res) => {
+    Post.deleteOne({ _id: req.params.id }, (err) => {
+      if (err) {
+        throw err;
+      }
+      res.status(200).json({ message: 'OK' });
+    });
+  },
+  
 };
 
 module.exports = PostsController;
--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,8 +1,40 @@
 import React from 'react';
 
 const Post = ({post}) => {
+
+   const handleDelete = async () =>{
+    const token = window.localStorage.getItem("token")
+    let response = await fetch(`/post/${post._id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify(null) 
+    })
+  //   if(response.status !== 204) {
+  //     console.log("bad request");
+
+  //   } else {
+  //     console.log("post was deleted")
+  //     let data = response.json()
+  //     window.localStorage.setItem("token", data.token);
+
+  //   }
+  }
+
+  // const clicked = () => {
+  
+  //   return console.log("clicked!!!!")
+  // }
+
+
   return(
-    <article data-cy="post" key={ post._id }>{ post.message }</article>
+    <div>
+      <article data-cy="post" key={ post._id }>{ post.message }{console.log(post)}</article>
+      <button onClick={handleDelete}>Delete post</button>
+    </div>
+    
   )
 }
 
